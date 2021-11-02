44158b347570461982f0a05b92ebdf39517a1a71
CB <42572919+D4anielCB@users.noreply.github.com>
2021-11-02 09:11:57 -0300

    Add files via upload

--- keybpm.py
+++ keybpm.py
@@ -1,12 +1,14 @@
 import sys, json, re, os, requests
 from urllib.request import urlopen, Request
 from urllib.parse import urlparse, quote_plus, unquote
+import eyed3
 
 fullpath = sys.argv[1]
 #fullpathnofile = 
 #print (sys.argv[1])
 file = fullpath.split('\\')
 file = file[len(file)-1]
+file = re.sub('_HP2-4BAND-3090_4band_1_', ' (', file, flags=re.IGNORECASE)
 extension = file.split('.')
 extension = extension[len(extension)-1]
 print("----------------------------------")
@@ -14,6 +16,8 @@ print(file)
 print("----------------------------------")
 #------------- Configuration ------------------
 Remixes = False # include remixes? False = no
+if re.compile("remixe?s?", re.IGNORECASE).findall(file):
+	Remixes = True
 Keys = {"C Major": "C", "C# Major": "C#", "D Major": "D"}
 Filename = "$artist - $song $type $key $bpm" # structure of the name of the file to be renamed
 MaxBPM = 140 # set the value max o a bpm
@@ -22,11 +26,42 @@ MaxBPM = 140 # set the value max o a bpm
 
 
 #----------------------------------------------
-def remove_accents(input_str):
-	import unicodedata
-	nfkd_form = unicodedata.normalize('NFKD', input_str)
-	only_ascii = nfkd_form.encode('ASCII', 'ignore')
-	return only_ascii.decode("utf-8") 
+def getimgtrack(url):
+	#requests.get(url)
+	r = requests.get(url)
+	return r.content
+def changetag(file,artist,song,album,genre,imagetrack):
+	if imagetrack:
+		imagetrack = getimgtrack(imagetrack)
+		#ST(imagetrack)
+	#try:
+	ST(file)
+	audiofile = eyed3.load(file)
+	artist = re.sub(' \,.+', '', artist, flags=re.IGNORECASE)
+	audiofile.tag.title = song
+	audiofile.tag.genre = genre
+	audiofile.tag.artist = artist
+	audiofile.tag.album = album
+	if imagetrack:
+		#fileimg = open("as345trfgsdf345345.jfif", 'rb').read()
+		#ST(imagetrack)
+		audiofile.tag.images.set(3, imagetrack, 'image/jpeg')
+		#audiofile.tag.images.set(3, imagetrack , "image/jpeg" ,u"a")
+		#audiofile.tag.images.set(type_=3, img_data=None, mime_type=None, description=u"", img_url=u"https://www.suasletras.com/fotos_artista/212af3ade52a5b519986ca5a200cd0b9.jpg")
+	#audiofile.tag.album_artist = "Various Artists"
+	#audiofile.tag.track_num = 3
+	audiofile.tag.save(version=eyed3.id3.ID3_V2_3)
+	#audiofile.tag.save()
+	#try:
+	#	os.remove("as345trfgsdf345345.jfif")
+	#except:
+	#	pass
+	#except:
+		#pass
+#----------------------------------------------
+def remove_accents(s):
+    filename = "".join(i for i in s if i not in "\/:*?<>|")
+    return filename
 #----------------------------------------------
 
 def ST(x="", o="w"):
@@ -68,7 +103,7 @@ def keys(value):
 #----------------------------------------------
 def bpm(value):
 	value = int(value)
-	if value > MaxBPM:
+	if value >= MaxBPM:
 		value = str(value/2)
 	else:
 		value = str(value)
@@ -79,7 +114,7 @@ fileq = re.sub('\(.+', '', file, flags=re.IGNORECASE) # variable to serach for t
 fileq = re.sub('^\d+( |\.|\-)', '', fileq, flags=re.IGNORECASE) # variable to serach for the song
 
 instrumental = re.compile("(instrumental|inst)", re.IGNORECASE).findall(file)
-acapella = re.compile("(aca|acca|acc?apell?a|pella)", re.IGNORECASE).findall(file)
+acapella = re.compile("(aca|acca|acc?apell?a|pella|vocals?)", re.IGNORECASE).findall(file)
 if instrumental:
 	steam="(Inst)"
 elif acapella:
@@ -155,20 +190,30 @@ if re.compile("\D+").findall(choice):
 trackid = newresults[int(choice)-1][1]
 track = OpenURL("https://musicstax.com/track/worst-nites/{0}".format(trackid)).replace("\r","").replace("\n","")
 trackname = re.compile("artist\: \"([^\"]+).+?song\: \"([^\"]+)").findall(track)
+image = re.compile("background: url\('([^\']+)").findall(track)
+imagetrack = ""
+if image:
+	imagetrack=image[0]
 keybpm = re.compile("eventAction\: \'BPM\',.+?eventLabel.+?(\d+).+?eventLabel\: \'([^\']+)").findall(track)
 #ST(keybpm)
 if trackname and keybpm:
+	ckeys=keys(keybpm[0][1])
+	cbpm=bpm(keybpm[0][0])
+	cartist = re.sub(' ?\,.+', '', trackname[0][0], flags=re.IGNORECASE)
+	csong = trackname[0][1]
 	#filename = "{0} - {1} (Inst) {3}".format(trackname[0][0], trackname[0][1], keybpm[0][0], keys(keybpm[0][1]))
 	#"$artist - $song ($type) $key $bpm" 
-	Filename = re.sub("\$artist", trackname[0][0], Filename, flags=re.IGNORECASE)
-	Filename = re.sub("\$song", trackname[0][1], Filename, flags=re.IGNORECASE)
-	Filename = re.sub("\$key", keys(keybpm[0][1]), Filename, flags=re.IGNORECASE)
-	Filename = re.sub("\$bpm", bpm(keybpm[0][0]), Filename, flags=re.IGNORECASE)
+	Filename = re.sub("\$artist", cartist, Filename, flags=re.IGNORECASE)
+	Filename = re.sub("\$song", csong, Filename, flags=re.IGNORECASE)
+	Filename = re.sub("\$key", ckeys, Filename, flags=re.IGNORECASE)
+	Filename = re.sub("\$bpm", cbpm, Filename, flags=re.IGNORECASE)
 	Filename = re.sub("\$type", steam, Filename, flags=re.IGNORECASE)
 	Filename = re.sub("  ", " ", Filename, flags=re.IGNORECASE)
 	#Filename
-print ("{0}.{1}".format(Filename,extension))
-os.rename(fullpath, "{0}.{1}".format(remove_accents(Filename),extension))
+	print ("{0}.{1}".format(Filename,extension))
+	changetag(fullpath,cartist,csong,cbpm,ckeys,imagetrack)
+	os.rename(fullpath, "{0}.{1}".format(remove_accents(Filename),extension))
+#file,artist,song,album
 #print (url)
 
 
